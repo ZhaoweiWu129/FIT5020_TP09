f765e01212f0eda57156e5857537c9653eb9fa0a
Sathwik Kallapur <satpk17@gmail.com>
2025-08-11 19:54:21 +1000

    correction of cloud vm instructions

--- update_docker_hub.sh
+++ update_docker_hub.sh
@@ -1,12 +1,12 @@
 docker compose build
-docker tag fit5020_onboarding_tp09-frontend 7wikmonash/onb_fe
+docker tag fit5020_onboarding_tp09-frontend:latest 7wikmonash/onb_fe
 docker tag fit5020_onboarding_tp09-backend:latest 7wikmonash/onb_be
-docker tag fit5020_onboarding_tp09-db 7wikmonash/onb_db
+docker tag fit5020_onboarding_tp09-db:latest 7wikmonash/onb_db
 # docker login
 docker push 7wikmonash/onb_fe:latest 
 docker push 7wikmonash/onb_be:latest
 docker push 7wikmonash/onb_db:latest
 
 ## on cloud VM:
-# docker pull compose pull
+# docker compose pull
 # docker compose up -d
